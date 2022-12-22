42d3f65601819f3337fafd0963a255bff9d09a6d
Eric Li <xiang.li@firstcommand.com>
2022-12-22 12:25:51 -0600

    refactor and add forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MaterialModule } from './material.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { RedeemCodeComponent } from './redeem-code/redeem-code.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PurchaseTicketComponent } from './purchase-ticket/purchase-ticket.component'
+import { PurchaseTicketComponent } from './purchase-ticket/purchase-ticket.component';
 
 @NgModule({
   declarations: [
@@ -26,11 +22,7 @@ import { PurchaseTicketComponent } from './purchase-ticket/purchase-ticket.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatGridListModule,
-    MatCardModule,
-    MatIconModule,
-    MatFormFieldModule,
-    MatInputModule,
+    MaterialModule,
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule
